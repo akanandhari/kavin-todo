e8b5cbed138ec181698a9d18ff9be4bd651c55c5
akanandhari <akanandhari@gmail.com>
2022-01-01 19:52:21 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
 import {NgbModule,NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 import { AddTodoComponent } from './add-todo/add-todo.component';
 
 
 @NgModule({
   imports:      [NgbModule,BrowserModule, FormsModule ,ReactiveFormsModule],
-  declarations: [ AppComponent, HelloComponent,AddTodoComponent ],
+  declarations: [ AppComponent,AddTodoComponent ],
   bootstrap:    [ AppComponent ],
+  entryComponents:[AddTodoComponent]
  
 })
 export class AppModule { }
